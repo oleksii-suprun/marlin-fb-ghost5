3abc6ecefaab4df35350a974fc498a535ab2212d
Oleksii Suprun <aleksey.suprun@gmail.com>
2023-05-28 13:31:26 +0200

    Print space changed to `255x195x195` for LimeHead

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1694,7 +1694,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 250
-#define Y_BED_SIZE 210
+#define Y_BED_SIZE 195
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1702,7 +1702,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 195
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
